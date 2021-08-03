f91e35f3bbe70013aeb67c6c719a434f8111b4c2
Anju Dhungana <anjudhungana@Anjus-MBP.MG8702>
2021-08-02 23:34:00 -0400

    added cartIcon and cartdropdown components to our application, also implemented cart reducer

--- src/components/directory/directory.component.jsx
+++ src/components/directory/directory.component.jsx
@@ -1,57 +1,58 @@
 import React from 'react';
+
 import MenuItem from '../menu-item/menu-item.component';
 
 import './directory.styles.scss';
 
-
-
 class Directory extends React.Component {
-    constructor() {
-        super();
+  constructor() {
+    super();
 
-        this.state = {
-           sections : [
-                {
-                  title: 'HATS',
-                  imageUrl: 'https://i.ibb.co/cvpntL1/hats.png',
-                  id: 1,
-                  linkUrl: 'hats'
-                },
-                {
-                  title: 'JACKETS',
-                  imageUrl: 'https://i.ibb.co/px2tCc3/jackets.png',
-                  id: 2,
-                  linkUrl: 'jackets'
-                },
-                {
-                  title: 'SHOES',
-                  imageUrl: 'https://i.ibb.co/0jqHpnp/sneakers.png',
-                  id: 3,
-                  linkUrl: 'shoes'
-                },
-                {
-                  title: 'WOMENS',
-                  imageUrl: 'https://i.ibb.co/GCCdy8t/womens.png',
-                  size: 'large',
-                  id: 4,
-                  linkUrl: 'womens'
-                },
-                {
-                  title: 'MENS',
-                  imageUrl: 'https://i.ibb.co/R70vBrQ/men.png',
-                  size: 'mens',
-                  id: 5,
-                }]
+    this.state = {
+      sections: [
+        {
+          title: 'hats',
+          imageUrl: 'https://i.ibb.co/cvpntL1/hats.png',
+          id: 1,
+          linkUrl: 'hats'
+        },
+        {
+          title: 'jackets',
+          imageUrl: 'https://i.ibb.co/px2tCc3/jackets.png',
+          id: 2,
+          linkUrl: ''
+        },
+        {
+          title: 'sneakers',
+          imageUrl: 'https://i.ibb.co/0jqHpnp/sneakers.png',
+          id: 3,
+          linkUrl: ''
+        },
+        {
+          title: 'womens',
+          imageUrl: 'https://i.ibb.co/GCCdy8t/womens.png',
+          size: 'large',
+          id: 4,
+          linkUrl: ''
+        },
+        {
+          title: 'mens',
+          imageUrl: 'https://i.ibb.co/R70vBrQ/men.png',
+          size: 'large',
+          id: 5,
+          linkUrl: ''
+        }
+      ]
     };
-}
+  }
 
-render() {
+  render() {
     return (
-        <div className='directory-menu'>
-        { this.state.sections.map(({id , ...otherSectionProps}) => (
-                <MenuItem key ={id} {...otherSectionProps}/>
-         ))}
-            </div>
+      <div className='directory-menu'>
+        {this.state.sections.map(({ id, ...otherSectionProps }) => (
+          <MenuItem key={id} {...otherSectionProps} />
+        ))}
+      </div>
     );
   }
 }
