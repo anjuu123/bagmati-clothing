b1b14576c6a8e635098e3c350a02cc49f48a2d87
Anju Dhungana <anjudhungana@Anjus-MBP.MG8702>
2021-08-05 00:17:24 -0400

    updating stripe integration and updating our title

---
+++ src/components/stripe-button/stripe-button.component.jsx
@@ -0,0 +1,29 @@
+import React from 'react';
+import StripeCheckout from 'react-stripe-checkout';
+
+const StripeCheckoutButton = ({ price }) => {
+  const priceForStripe = price * 100;
+  const publishableKey = 'pk_test_51IqlynDMzUpLbsMURkxhdzBWLWf8aUUUhYhdWF6GauFVblqDlU012lheG65onHTVeUvHLmgCd7bJA02UtoPlUxS100kU8Xx9nZ';
+
+  const onToken = token => {
+    console.log(token);
+    alert('Payment Succesful!');
+  };
+
+  return (
+    <StripeCheckout
+      label='Pay Now'
+      name='Bagmati Clothing Ltd.'
+      billingAddress
+      shippingAddress
+      image='https://svgshare.com/i/CUz.svg'
+      description={`Your total is $${price}`}
+      amount={priceForStripe}
+      panelLabel='Pay Now'
+      token={onToken}
+      stripeKey={publishableKey}
+    />
+  );
+};
+
+export default StripeCheckoutButton;
